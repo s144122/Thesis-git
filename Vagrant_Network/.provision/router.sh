03bdca766d70fc6d859668edb9232987ec756eba
s144122@student.dtu.dk <s144122@student.dtu.dk>
2020-03-09 19:11:25 +0100

    Stuff, doens't work

--- Vagrant_Network/.provision/router.sh
+++ Vagrant_Network/.provision/router.sh
@@ -3,15 +3,15 @@
 # Update Packages
 apt-get update
 sudo apt-get install iptables
-sysctl net.ipv4.ip_forward=1
+#sysctl net.ipv4.ip_forward=1
 
 
-iptables-F
+#iptables-F
 
 #iptables -t nat -A OUTPUT -d 10.0.0.20 -j DNAT --to-destination 10.0.0.10
-iptables -t nat -A POSTROUTING -o ethx -j MASQUERADE
+#iptables -t nat -A POSTROUTING -o ethx -j MASQUERADE
 
-iptables-save
+#iptables-save
 
 
 
--- Vagrant_Network/VPN-client/vpnsetup.sh
+++ Vagrant_Network/VPN-client/vpnsetup.sh
@@ -1,34 +1,25 @@
-
-
-#sudo apt-get -y install dos2unix
-#sudo dos2unix -n /usr/shared/vpnsetup.sh /usr/shared/vpnsetupUnix.sh
-
 #sudo -u vagrant -H bash <<EOF
-cd /home/vagrant/
-echo $PWD
+
 sudo apt-get -y update
 sudo apt-get -y install openvpn easy-rsa
-make-cadir ~/openvpn-ca
-cd ~/openvpn-ca
-#cp /usr/shared/vars /home/vagrant/openvpn-ca/
-#mv /home/vagrant/openvpn-ca/openssl-1.0.0.cnf /home/vagrant/openvpn-ca/openssl.cnf
-cp /usr/shared/vars ~/openvpn-ca/
-cat vars
-mv openssl-1.0.0.cnf openssl.cnf
-echo $PWD
-source vars
-./clean-all
-./build-ca
+make-cadir /home/vagrant/openvpn-ca
+#sudo cd ~/openvpn-ca
+cp /usr/shared/vars /home/vagrant/openvpn-ca/
+mv /home/vagrant/openvpn-ca/openssl-1.0.0.cnf /home/vagrant/openvpn-ca/openssl.cnf
+/home/vagrant/openvpn-ca/source vars
+/home/vagrant/openvpn-ca/clean-all
+
+
+#/home/vagrant/openvpn-ca/build-ca
 #/home/vagrant/openvpn-ca/build-key-server vpnserver -y
 #/home/vagrant/openvpn-ca/build-dh
 #openvpn --genkey --secret keys/ta.key
 #source vars
 #/home/vagrant/openvpn-ca/build-key client1
 #cd ~/openvpn-ca/keys
-#echo $PWD
 #sudo cp ca.crt ca.key vpnserver.crt vpnserver.key ta.key dh2048.pem /etc/openvpn
 #gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz | sudo tee /etc/openvpn/server.conf
-#cp /usr/shared/server.conf /etc/openvpn/
+
 #sudo nano /etc/openvpn/server.conf
 
 #EOF
